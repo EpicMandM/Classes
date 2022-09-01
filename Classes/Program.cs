e7449627d85d07448594ae6b9bffa976934689e8
Vladyslav Zhukov <epicmand.mi@gmail.com>
2022-09-01 19:06:16 +0300

    Implemented Main

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     public static void Main(string[] args)
     {
+        M590 m590 = new(50);
+        Console.WriteLine($"{nameof(m590.WeaponType)} is {m590.WeaponType}, \n" +
+            $"the amount of {nameof(m590.Ammo).ToLower()} is {m590.Ammo}\n");
+        m590.Shoot();
 
+        Car car = new Car(10000);
+        car.Go();
     }
 }
